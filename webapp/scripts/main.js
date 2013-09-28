460a08dc0d1be7251e6a9ac9efe045f40c987f72
Bill Cauchois <wcauchois@gmail.com>
2013-09-28 16:48:24 -0400

    THREE JS skeleton

--- webapp/scripts/main.js
+++ webapp/scripts/main.js
@@ -2,11 +2,24 @@
 BB.namespace('BB.main');
 
 BB.main.Application = BB.Class.extend({
+  initialize: function() {
+    this.camera = new THREE.PerspectiveCamera(
+      75, window.innerWidth / window.innerHeight, 1, 10000);
+    this.scene = new THREE.Scene();
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  },
   run: function() {
+    document.body.appendChild(this.renderer.domElement);
+  }
+}, {
+  getInstance: function() {
+    if (!BB.main.Application.instance_) {
+      BB.main.Application.instance_ = new BB.main.Application();
+    }
+    return BB.main.Application.instance_;
   }
 });
 
-console.log(BB.foo.bar); // XXX
-
-new BB.main.Application().run();
+BB.main.Application.getInstance().run();
 
